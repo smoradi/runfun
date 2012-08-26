d56be7f5fcd5f87a14253304eb993f700350642a
Saeid Moradi <saeid3@gmail.com>
2012-08-26 18:27:08 +0000

    roo> web mvc scaffold --class ~.web.controller.UserController --backingType ~.domain.User |executed

---
+++ src/main/java/com/mycompany/runfun/web/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.mycompany.runfun.web.controller;
+
+import com.mycompany.runfun.domain.User;
+import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/users")
+@Controller
+@RooWebScaffold(path = "users", formBackingObject = User.class)
+public class UserController {
+}
