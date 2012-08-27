2f1bff6804f10d571a4087663af7c8668365e2ac
Saeid Moradi <saeid3@gmail.com>
2012-08-27 07:23:44 +0000

    roo> field string --fieldName name --notNull roo> field number --fieldName distance --type java.lang.Double --notNull roo> field reference --fieldName user --type ~.domain.User --joinColumnName user_id --notNull --cardinality MANY_TO_ONE --permitReservedWords |executed

--- src/main/java/com/mycompany/runfun/domain/Land.java
+++ src/main/java/com/mycompany/runfun/domain/Land.java
@@ -1,5 +1,8 @@
 package com.mycompany.runfun.domain;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.tostring.RooToString;
@@ -8,4 +11,15 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooToString
 @RooJpaActiveRecord
 public class Land {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Double distance;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
